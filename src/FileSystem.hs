8d09286b82501031c9f703a47e7bdc6a75a19b4c
Stefan Risberg <steffenomak@gmail.com>
2015-12-31 10:20:26 +0100

    Made filesystem scan simpler.

--- src/FileSystem.hs
+++ src/FileSystem.hs
@@ -1,101 +1,67 @@
-module FileSystem
-( fileStatus
-, directoryContent
-, filterFiles
-, filterDirectorys
-, getAllFiles
-) where
+{-# LANGUAGE TemplateHaskell #-}
 
-import           Types.File            (File (..))
-import qualified Types.File       as F
-import qualified Types.Permission as P
+module FileSystem
+( getFilePaths
+, getFiles
+, fileSource
+) where
 
 import           System.FilePath       (( </> ))
 import           System.Directory      (getDirectoryContents
                                        ,canonicalizePath
                                        )
-import qualified System.Posix.Files as P
-import           System.Posix.Files    (isDirectory
-                                       ,FileStatus
-                                       ,accessTimeHiRes
-                                       ,modificationTimeHiRes
-                                       ,fileSize
-                                       ,isRegularFile
-                                       )
-import           System.IO.Error       (isDoesNotExistError
-                                       ,catchIOError
+
+import           System.Posix.Files    (isRegularFile
+                                       ,isDirectory
+                                       ,getSymbolicLinkStatus
                                        )
 
-import           Control.Monad         (liftM)
+import           Control.Monad (liftM)
+import           Control.Monad.IO.Class
 
-import           Data.Time.Clock.POSIX (posixSecondsToUTCTime)
-import           Data.Maybe            (fromJust)
+import           Data.Conduit
+import           Data.Conduit.List (consume)
+import           Data.Maybe (fromJust)
 
-fileStatus :: FilePath
-           -> IO (Maybe FileStatus)
-fileStatus fp =
-    catchIOError (P.getFileStatus fp >>= return . Just)
-                 (\e -> if isDoesNotExistError e
-                            then return Nothing
-                            else ioError e)
+import           Types.File (File, conv)
 
--- |Strict directory and status fetcher. Returns empty if no content or not a
--- directory.
-directoryContent :: FilePath
-              -> IO [(FilePath, FileStatus)]
-directoryContent fp = do
-    let filterDots :: FilePath
-                   -> Bool
-        filterDots "." = False
-        filterDots ".." = False
-        filterDots _ = True
+io :: MonadIO m
+   => IO a
+   -> m a
+io = liftIO
 
-    is <- liftM (maybe False isDirectory) (fileStatus fp)
-    if is
-        then (do cont <- liftM (map (fp </>) . filter filterDots)
-                               (getDirectoryContents fp)
-                 stat <- mapM (liftM fromJust . fileStatus) cont
-                 return $! zip cont stat)
-        else return []
+filterDots :: [FilePath]
+           -> [FilePath]
+filterDots = filter (\a -> a `notElem` [".", ".."])
 
--- |Get all files.
-filterFiles :: [(FilePath, FileStatus)] -- ^ List to filter.
-            -> [File] -- ^ List of all files.
-filterFiles = let aT = posixSecondsToUTCTime . accessTimeHiRes
-                  mT = posixSecondsToUTCTime . modificationTimeHiRes
-              in map (\(fp, fs) -> File { F._id = Nothing
-                                        , F._path = fp
-                                        , F._size = fromIntegral (fileSize fs)
-                                        , F._accessTime = aT fs
-                                        , F._modTime = mT fs
-                                        , F._user = P.Permission True True False
-                                        , F._group = P.Permission True True False
-                                        , F._other = P.Permission True False False
-                                        })
-               . filter (isRegularFile . snd)
+getFilePaths :: FilePath
+             -> IO [FilePath]
+getFilePaths fp = fileSource fp $$ consume
 
--- |Get all folders.
-filterDirectorys :: [(FilePath, FileStatus)] -- ^ List to filter.
-                 -> [FilePath] -- ^ List of all files.
-filterDirectorys = map fst . filter (isDirectory . snd)
+getFiles :: FilePath
+         -> IO [File]
+getFiles fp =
+    let fpToFile :: MonadIO m => Conduit FilePath m File
+        fpToFile = await
+                   >>= maybe (return ())
+                             (\f -> do fs <- io (getSymbolicLinkStatus f)
+                                       yield (fromJust $ conv f fs)
+                                       fpToFile)
+    in fileSource fp $= fpToFile $$ consume
 
--- |Get all files conteind in folder and its subfolders.
-getAllFiles :: FilePath
-            -> IO [File]
-getAllFiles fp =
-    let split :: [(FilePath, FileStatus)]
-              -> ([File], [FilePath])
-        split x = (filterFiles x, filterDirectorys x)
-
-        get' :: [File]
-             -> [FilePath]
-             -> IO [File]
-        get' files [] = return files
-        get' f (d:dirs) = do cont <- directoryContent d
-                             let (f', d') = split cont
-                             get' (f ++ f') (dirs ++ d')
-    in do content <- canonicalizePath fp
-                     >>= directoryContent
-          let (f, d) = split content
-          get' f d
-
+fileSource :: MonadIO m
+           => FilePath
+           -> Source m FilePath
+fileSource fp =
+    let cont :: MonadIO m => [FilePath] -> Source m FilePath
+        cont [] = return ()
+        cont (f:fs) =
+            let handFile = yield f >> cont fs
+                handDir = liftM filterDots (io . getDirectoryContents $ f)
+                          >>= return . map (f </>)
+                          >>= \c -> cont (fs ++ c)
+                hand s | isRegularFile s = handFile
+                       | isDirectory s   = handDir
+                       | otherwise       = cont fs
+            in io (getSymbolicLinkStatus f) >>= hand
+    in io (canonicalizePath fp) >>= \f -> cont [f]
