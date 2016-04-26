8665ff8296175eb9188ee6cf537652d8a4ebe3c5
Stefan Risberg <steffenomak@gmail.com>
2016-04-26 21:48:34 +0200

    Made hash use Jobs.

--- src/Hash.hs
+++ src/Hash.hs
@@ -1,14 +1,16 @@
-module Hash
-( hash
-) where
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Hash where
 
 import           Data.Conduit ( Sink
+                              , Source
                               , await
+                              , yield
                               , ($$))
 
-import           Data.Conduit.Binary (sourceFile)
 import           Data.LargeWord (Word128)
 import           Data.ByteString (ByteString)
+import qualified Data.ByteString as B
 import           Data.Binary (decode)
 import           Data.ByteString.Lazy (fromStrict)
 
@@ -17,21 +19,103 @@ import           Crypto.Hash.MD5 ( init
                                  , finalize
                                  , Ctx)
 
-import           Control.Monad.Trans.Resource
+import           Control.Lens hiding (at)
+import           Control.Monad.Reader
+import           Control.Monad.Except
+
+import           System.IO ( openFile
+                           , IOMode (ReadMode)
+                           , hClose
+                           , hSeek
+                           , SeekMode (AbsoluteSeek)
+                           , Handle
+                           )
 import           Prelude hiding (init)
 
-createHash :: MonadResource m
-           => Sink ByteString m Word128
-createHash =
-    let c' :: MonadResource m
-           => Ctx
-           -> Sink ByteString m Word128
-        c' ctx = do
-            await
-            >>= maybe (return $! decode $ fromStrict $ finalize ctx)
-                      (\b -> c' (update ctx b))
-    in c' init
+import           Job
 
-hash :: FilePath
-     -> IO Word128
-hash fp = runResourceT $ sourceFile fp $$ createHash
+data HashJob
+    = HashJob
+        { _idL :: Int -- ^ Id of file.
+        , _fileL :: FilePath -- ^ File to hash.
+        , _fileAtL :: Int -- ^ How much of the file have been consumed.
+        , _ctxL :: Ctx -- ^ Hash context.
+        , _finL :: Ctx -> ByteString -- ^ Finalization of hash.
+        , _updateL :: Ctx -> ByteString -> Ctx -- ^ Update context.
+        }
+
+makeLenses ''HashJob
+
+-- | Create a new HashJob.
+newHashJob :: Int -- ^ File id.
+           -> FilePath -- ^ File to hash.
+           -> HashJob
+newHashJob i f = HashJob { _idL = i
+                         , _fileL = f
+                         , _fileAtL = 0
+                         , _ctxL = init
+                         , _finL = finalize
+                         , _updateL = update
+                         }
+
+-- | Reads a file with 4096 bytestrings.
+readBlock :: FilePath -- ^ File to start reading.
+          -> Maybe Int -- ^ Where to start reading from.
+          -> Source (Job HashJob) ByteString
+readBlock fp at = do
+    hand <- lift . ioJob $! openFile fp ReadMode
+
+    lift . ioJob $ maybe (return ())
+                         (hSeek hand AbsoluteSeek . fromIntegral)
+                         at
+
+    run hand
+    lift . ioJob $ hClose hand
+    return ()
+    where
+        -- | Reads a block. Returns Nothing when the handle is eof.
+        consumeHand :: Handle -> IO (Maybe ByteString)
+        consumeHand h = do
+            b <- B.hGet h 4096
+            return $ if B.null b
+                then Nothing
+                else Just b
+
+        run :: Handle
+            -> Source (Job HashJob) ByteString
+        run h = do
+            lift (ioJob $ consumeHand h)
+            >>= maybe (return ())
+                      (\bs -> yield bs >> run h)
+
+-- | Consumes bytestring source and creates a hash. Can be stopped.
+hashSink :: HashJob -- ^ Hash state.
+         -> Sink ByteString (Job HashJob) (Either HashJob Word128)
+hashSink j = do
+    --halt <- liftIO p
+    lift (pauseJob j)
+    await >>= maybe end hash
+
+    where
+        hash bs = let len = B.length bs
+                      ctx = (j ^. updateL) (j ^. ctxL) bs
+                      j' = j & fileAtL .~ (len + (j ^. fileAtL))
+                             & ctxL .~ ctx
+                  in hashSink j'
+        end = return $! Right (decode . fromStrict . (j ^. finL) $ j ^. ctxL)
+
+-- | Runs a HashJob.
+runHashJob :: HashJob
+           -> IO (JobKey HashJob Word128)
+runHashJob j =
+    runJob job
+
+    where
+        job :: Job HashJob Word128
+        job = do
+            r <- readBlock (j ^. fileL) (Just $ j ^. fileAtL)
+              $$ hashSink j
+
+            case r of
+                Left x -> Job $ throwError x
+                Right x -> return x
