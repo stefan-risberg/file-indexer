f967ed808a02bb2ad4975a14da0185fe8c09dac9
Stefan Risberg <steffenomak@gmail.com>
2016-04-26 21:49:40 +0200

    Updated database with a job table.

--- src/Database.hs
+++ src/Database.hs
@@ -12,6 +12,7 @@ import Database.Persist.TH
 import Data.Text (Text)
 import Data.Word (Word8, Word64)
 import Data.Time.Clock (UTCTime)
+import Data.ByteString (ByteString)
 
 share [mkPersist sqlSettings { mpsGenerateLenses = True }, mkMigrate "fileIndex"] [persistLowerCase|
 File
@@ -24,6 +25,12 @@ File
     groupPer   Word8
     otherPer   Word8
     UniquePath location name
+    deriving   Show
+
+Job
+    file       FileId
+    ctx        ByteString
+    consumed   Int
     deriving   Show
 
 Hash
